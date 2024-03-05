e55a4b72e809749633b11ccd287081e6ebf0e53a
mukul-kr <mukul0000kumar@gmail.com>
2024-03-05 15:21:07 +0530

    Add docs validation code by comparing user and password

---
+++ notification/domain/usecase/validation.py
@@ -0,0 +1,20 @@
+import secrets
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBasicCredentials, HTTPBasic
+
+security = HTTPBasic()
+
+
+def get_current_username(
+    credentials: HTTPBasicCredentials = Depends(security),
+):
+    correct_username = secrets.compare_digest(credentials.username, "user")
+    correct_password = secrets.compare_digest(credentials.password, "password")
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
