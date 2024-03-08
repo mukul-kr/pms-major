19ac7eda1b99f9f34ecd958d78db40012bbd6f97
sonali-rajput <sonalirajput1088@gmail.com>
2024-03-08 12:59:37 +0530

    Add possible base exception

---
+++ notification/exception/email.py
@@ -0,0 +1,5 @@
+class EmailException(Exception):
+    def __init__(self, message, status_code=401):
+        self.message = message
+        self.status_code = status_code
+        super().__init__(self.message)
